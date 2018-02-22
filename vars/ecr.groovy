a4de904005f9ec44e49f51bde39061736cc6b71f
Daisuke Ito <ito@wacul.co.jp>
2018-02-22 17:16:16 +0900

    fix shell command

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -1,4 +1,16 @@
-def addTag(String awsRegistryId, String repository, String sourceTag, String addTag) {
-    def imageManifest = sh(script: aws ecr batch-get-image --registry-id "${awsRegistryId}" --repository-name "${repository}" --image-ids "imageTag=${sourceTag}" --query images[].imageManifest --output text|head -c -1).text
-    sh(script: aws ecr put-image --registry-id "${awsRegistryId}" --repository-name "${repository}" --image-tag "${addTag}" --image-manifest "${imageManifest}", returnStdout: true)
+def addTag(Map settings) {
+    def imageManifest = ['aws', 'ecr', 'batch-get-image', '--registry-id', "${settings.awsRegistryId}", '--repository-name', "${settings.repository}", "--image-ids", "imageTag=\"${settings.sourceTag}\"", '--query', 'images[].imageManifest', '--output', 'text'].execute()
+    imageManifest.waitFor()
+    if (imageManifest.exitValue()) {
+        println imageManifest.err.text
+        throw new Exception("Cannot retrieve image manifest from ECR.")
+    }
+    def manifestText = imageManifest.text
+    def manifestJson = manifestText.substring(0, manifestText.length() - 1)
+    def addTag = ['aws', 'ecr', 'put-image', '--registry-id', "${settings.awsRegistryId}", '--repository-name', "${settings.repository}", '--image-tag', "${settings.addTag}", '--image-manifest', "${manifestJson}"].execute()
+    addTag.waitFor()
+    if (addTag.exitValue()) {
+        println addTag.err.text
+        throw new Exception("Cannot put image manifest to ECR.")
+    }
 }
